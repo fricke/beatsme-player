abfd3590184ab6df50b3db6824eb2c577dc84ea0
Chris Fricke <chris.fricke@gmail.com>
2014-07-15 01:52:12 -0700

    Updates for core-ajax and more tinkering around

--- beatsme-player.js
+++ beatsme-player.js
@@ -3,5 +3,8 @@ Polymer('beatsme-player', {
   playerData : '',
   ready: function() {
     this.data='parent content';
+  },
+  ajaxResponse: function(event, response) {
+            console.log(response);
   }
 });
