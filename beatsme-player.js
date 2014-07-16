8df2f87bbac8626fc78a0a43b6579bf8a781cd46
Chris Fricke <chris.fricke@gmail.com>
2014-07-16 00:14:45 -0700

    Basic scaffolding and html goodies

---
+++ beatsme-player.js
@@ -0,0 +1,48 @@
+
+Polymer('beatsme-player', {
+  ready: function() {
+    this.data='parent content';
+  },
+  ajaxResponse: function(event, response) {
+    var that = this;
+    var bam = new BeatsAudioManager("beatsme");
+
+    bam.on("ready", handleReady);
+    bam.on("error", handleError);
+    function handleReady(value) {
+        bam.clientId = that.clientId;
+        bam.authentication = {
+            access_token:that.accessToken,
+            user_id:that.userId
+        };
+        var item = response.response.data[that.playerIndex];
+        bam.identifier = this.trackId;
+        //bam.load();
+    };
+    function handleError(value) {
+        switch(value){
+            case "auth":
+            // Beats Music API auth error (401)
+            break;
+            case "connectionfailure":
+            // audio stream connection failure
+            break;
+            case "apisecurity":
+            // Beats Music API crossdomain error
+            break;
+            case "streamsecurity":
+            // audio stream crossdomain error
+            break;
+            case "streamio":
+            // audio stream io error
+            break;
+            case "apiio":
+            // Beats Music API io error getting track data
+            break;
+            case "flashversion":
+            // flash version too low or not installed
+            break;
+        }
+    };
+  }
+});
